d6b807fbe47bfc5c698585898f6f7d27d183bbac
unknown <xu_junkai@foxmail.com>
2019-03-01 15:55:23 +0800

    123

--- roles/knowage/files/init.sh
+++ roles/knowage/files/init.sh
@@ -3,6 +3,7 @@
 old_password=$(cat /root/password.txt | awk -F ":" '{print $2}' )
 new_password=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c10)
 
+sleep 5s
 systemctl restart mysql
 mysqladmin -uroot -p${old_password} password $new_password
 
@@ -10,6 +11,7 @@ echo 'Databases root Password:'$new_password  > /root/password.txt
 
 sed -i "s/123456/$new_password/g" /data/wwwroot/Knowage-Server-CE/conf/server.xml
 
+systemctl restart knowage.service
 sed -i "s/\/root\/init.sh//" /etc/rc.local
 rm -rf /root/init.sh
 
