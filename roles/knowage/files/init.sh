233a8d56a778a09dc48d3c82aeeaf51a11d5aab9
Mozlk <xu_junkai@foxmail.com>
2019-06-26 15:45:18 +0800

    123

--- roles/knowage/files/init.sh
+++ roles/knowage/files/init.sh
@@ -11,9 +11,7 @@ echo -e 'MySQL username:root\nMySQL Password:'$new_password  > /credentials/pass
 
 sed -i "s/$old_password/$new_password/g" /data/wwwroot/Knowage-Server-CE/conf/server.xml
 
-systemctl restart knowage.service
-userdel -rf websoft9      
-rm -rf /var/db/sudo/lectured/websoft9                                          
+systemctl restart knowage.service                                       
 sed -i "s/\/root\/init.sh//" /etc/rc.local                                                      
 rm -rf /root/init.sh
 
